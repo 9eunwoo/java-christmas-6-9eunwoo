82dac9aa1cf7126a4595a8b2d4c36dda7c03717b
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-11-10 09:53:39 +0900

    feat(domain): 메뉴 항목 열거형 추가

--- src/test/java/christmas/domain/MenuItemTest.java
+++ src/test/java/christmas/domain/MenuItemTest.java
@@ -7,34 +7,35 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
 public class MenuItemTest {
+    @DisplayName("메뉴 각 항목의 이름, 가격, 카테고리를 정확히 반환하는지 테스트")
     @ParameterizedTest
     @EnumSource(MenuItem.class)
     public void givenMenuItem_whenGetDetails_thenCorrectDetailsReturned(MenuItem menuItem) {
         // when
-        String nameInKorean = menuItem.getNameInKorean();
+        String name = menuItem.getName();
         int price = menuItem.getPrice();
-        MenuCategory category = menuItem.getCategory();
+        Category category = menuItem.getCategory();
 
         // then
         if (menuItem == MenuItem.MUSHROOM_SOUP) {
-            assertThat(nameInKorean).isEqualTo("양송이수프");
+            assertThat(name).isEqualTo("양송이수프");
             assertThat(price).isEqualTo(6_000);
-            assertThat(category).isEqualTo(MenuCategory.APPETIZER);
+            assertThat(category).isEqualTo(Category.APPETIZER);
         }
         if (menuItem == MenuItem.TBONE_STEAK) {
-            assertThat(nameInKorean).isEqualTo("티본스테이크");
+            assertThat(name).isEqualTo("티본스테이크");
             assertThat(price).isEqualTo(55_000);
-            assertThat(category).isEqualTo(MenuCategory.MAIN_DISH);
+            assertThat(category).isEqualTo(Category.MAIN);
         }
         if (menuItem == MenuItem.CHOCO_CAKE) {
-            assertThat(nameInKorean).isEqualTo("초코케이크");
+            assertThat(name).isEqualTo("초코케이크");
             assertThat(price).isEqualTo(15_000);
-            assertThat(category).isEqualTo(MenuCategory.DESSERT);
+            assertThat(category).isEqualTo(Category.DESSERT);
         }
         if (menuItem == MenuItem.ZERO_COLA) {
-            assertThat(nameInKorean).isEqualTo("제로콜라");
+            assertThat(name).isEqualTo("제로콜라");
             assertThat(price).isEqualTo(3_000);
-            assertThat(category).isEqualTo(MenuCategory.BEVERAGE);
+            assertThat(category).isEqualTo(Category.BEVERAGE);
         }
     }
 }
