d67482b9cc66aa6784408d9f19b942c915393c3b
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-11-10 12:03:30 +0900

    feat(domain): 중복 메뉴 주문 시 예외 처리 구현

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -4,20 +4,23 @@ import java.util.EnumMap;
 import java.util.Map;
 
 public class Order {
-     private final Map<MenuItem, Integer> orderItems;
+    private final Map<MenuItem, Integer> orderItems;
 
     public Order() {
         this.orderItems = new EnumMap<>(MenuItem.class);
     }
 
     public void addItem(MenuItem item, int quantity) {
+        if (orderItems.containsKey(item)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
         this.orderItems.put(item, quantity);
     }
 
     public int getTotalPrice() {
         return orderItems.entrySet()
-                         .stream()
-                         .mapToInt(entry -> entry.getKey().getPrice() * entry.getValue())
-                         .sum();
+                .stream()
+                .mapToInt(entry -> entry.getKey().getPrice() * entry.getValue())
+                .sum();
     }
 }
