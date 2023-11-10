96b8b53476aea2db3d6416c7b9b7de39ad5c6a94
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-11-10 11:05:30 +0900

    test(domain): 메뉴 주문 및 총 주문 금액 계산 테스트 추가

---
+++ src/test/java/christmas/domain/Order.java
@@ -0,0 +1,25 @@
+package christmas.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Order {
+    @DisplayName("주문한 메뉴의 총 가격을 정확히 계산하는지 테스트")
+    @Test
+    public void addItem_ShouldCorrectlyUpdateTotalPrice() {
+        // given
+        Order order = new Order();
+        MenuItem item = MenuItem.MUSHROOM_SOUP;
+        int quantity = 2;
+        int expected = item.getPrice() * quantity;
+
+        // when
+        order.addItem(item, quantity);
+        int actual = order.getTotalPrice();
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+}
