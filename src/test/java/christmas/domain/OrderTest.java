6ff658eb41c0b0b6a3853801189a925ca8460d7b
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-11-11 04:44:51 +0900

    특정 카테고리에 속하는 음식 수량 계산 테스트

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -5,15 +5,23 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import christmas.Constant.Message;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class OrderTest {
+    private Order order;
+
+    @BeforeEach
+    void setUp() {
+        order = new Order();
+    }
+
     @DisplayName("주문한 메뉴의 총 가격을 정확히 계산하는지 테스트")
     @Test
-    public void addItem_ShouldCorrectlyUpdateTotalPrice() {
+    void addItem_ShouldCorrectlyUpdateTotalPrice() {
         // given
-        Order order = new Order();
         MenuItem item = MenuItem.MUSHROOM_SOUP;
         int quantity = 2;
         int expected = item.getPrice() * quantity;
@@ -28,9 +36,8 @@ public class OrderTest {
 
     @DisplayName("중복 메뉴를 주문할 경우, 예외를 발생시키는지 테스트")
     @Test
-    public void addItem_ShouldThrowException_WhenDuplicateItemAdded() {
+    void addItem_ShouldThrowException_WhenDuplicateItemAdded() {
         // given
-        Order order = new Order();
         MenuItem item = MenuItem.MUSHROOM_SOUP;
         int quantity = 2;
         order.addItem(item, quantity);
@@ -43,9 +50,8 @@ public class OrderTest {
 
     @DisplayName("메뉴의 수량이 0개 이하일 경우, 예외를 발생시키는지 테스트")
     @Test
-    public void addItem_ShouldThrowException_WhenQuantityIsZeroOrNegative() {
+    void addItem_ShouldThrowException_WhenQuantityIsZeroOrNegative() {
         // given
-        Order order = new Order();
         MenuItem item = MenuItem.MUSHROOM_SOUP;
         int quantity = 0;
 
@@ -57,9 +63,8 @@ public class OrderTest {
 
     @DisplayName("주문한 메뉴의 총 수량이 20개를 초과할 경우, 예외를 발생시키는지 테스트")
     @Test
-    public void finalizeOrder_ShouldThrowException_WhenTotalQuantityIsOverTwenty() {
+    void finalizeOrder_ShouldThrowException_WhenTotalQuantityIsOverTwenty() {
         // given
-        Order order = new Order();
         MenuItem item = MenuItem.MUSHROOM_SOUP;
         int quantity = 21;
         order.addItem(item, quantity);
@@ -72,9 +77,8 @@ public class OrderTest {
 
     @DisplayName("주문한 메뉴의 총 수량이 20개 이하일 경우, 정상적으로 주문을 완료하는지 테스트")
     @Test
-    public void finalizeOrder_ShouldSuccessfullyFinalizeOrder_WhenTotalQuantityIsLessThanTwenty() {
+    void finalizeOrder_ShouldSuccessfullyFinalizeOrder_WhenTotalQuantityIsLessThanTwenty() {
         // given
-        Order order = new Order();
         MenuItem item = MenuItem.MUSHROOM_SOUP;
         int quantity = 20;
         order.addItem(item, quantity);
@@ -85,9 +89,8 @@ public class OrderTest {
 
     @DisplayName("음료만 주문한 경우, 예외를 발생시키는지 테스트")
     @Test
-    public void finalizeOrder_ShouldThrowException_WhenOnlyBeverageOrdered() {
+    void finalizeOrder_ShouldThrowException_WhenOnlyBeverageOrdered() {
         // given
-        Order order = new Order();
         MenuItem item = MenuItem.ZERO_COLA;
         int quantity = 20;
         order.addItem(item, quantity);
@@ -98,11 +101,10 @@ public class OrderTest {
                 .hasMessageContaining(Message.ERROR_INVALID_ORDER.getMessage());
     }
 
-    @DisplayName("음료를 포함한 다른 메뉴를 주문한 경우, 정상적으로 주문을 완료하는지 테스트")
+    @DisplayName("주문한 메뉴의 총 수량이 20개 이하이고, 음료를 포함한 다른 메뉴를 주문한 경우, 정상적으로 주문을 완료하는지 테스트")
     @Test
-    public void finalizeOrder_ShouldSuccessfullyFinalizeOrder_WhenBeverageAndOtherItemsOrdered() {
+    void finalizeOrder_ShouldSuccessfullyFinalizeOrder_WhenBeverageAndOtherItemsOrdered() {
         // given
-        Order order = new Order();
         MenuItem item = MenuItem.ZERO_COLA;
         int quantity = 19;
         order.addItem(item, quantity);
@@ -112,5 +114,22 @@ public class OrderTest {
 
         // when & then
         assertThatCode(() -> order.finalizeOrder()).doesNotThrowAnyException();
+    }
+
+    @DisplayName("특정 카테고리에 속하는 음식의 수량을 정확히 계산하는지 테스트")
+    @Test
+    void getTotalQuantityByCategory_ShouldCorrectlyCalculateTotalQuantityByCategory() {
+        // given
+        MenuItem item;;
+        int totalAppetizerQuantity = 3;
+        order.addItem(MenuItem.MUSHROOM_SOUP, 2);
+        order.addItem(MenuItem.TAPAS, 1);
+        order.addItem(MenuItem.ZERO_COLA, 3);
+
+        // when
+        int actual = order.getTotalQuantityByCategory(Category.APPETIZER);
+
+        // then
+        assertThat(actual).isEqualTo(totalAppetizerQuantity);
     }
 }
