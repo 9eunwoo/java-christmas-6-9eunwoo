e63e2bb4f09f2da762267731694ee6badcf35f16
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-11-11 06:20:43 +0900

    할인 정책에 따른 할인 금액 계산 테스트 정리

--- src/test/java/christmas/domain/DiscountPolicyTest.java
+++ src/test/java/christmas/domain/DiscountPolicyTest.java
@@ -7,26 +7,28 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class DiscountPolicyTest {
-    @DisplayName("할인 정책에 따라 할인 금액을 정확히 계산하는지 테스트")
+    @DisplayName("할인 정책에 따라 할인 금액이 정확히 계산되는지 확인한다.")
     @Test
     void givenDiscountPolicy_whenCalculateDiscount_thenCorrectDiscountCalculated() {
         // given
         Order order = new Order();
         Calendar calendar = Calendar.getInstance();
-        calendar.set(2023, Calendar.DECEMBER, 25); // 2023.12.25 (월) 크리스마스
+        int dayOfMonth = 25;
+        calendar.set(2023, Calendar.DECEMBER, dayOfMonth); // 2023.12.25 (월) 크리스마스
         MenuItem item = MenuItem.CHOCO_CAKE; // 초코케이크는 디저트
         int quantity = 2;
         order.addItem(item, quantity);
         order.finalizeOrder();
-        int expectedDiscount = 1000 + 100 * (25 - 1); // 2023.12.1 ~ 25는 크리스마스 디데이 할인 적용 o
-        expectedDiscount += 2023 * quantity; // 주중 할인(일~목, 디저트) o
-        expectedDiscount += 0; // 주말 할인(금~토, 메인) x
-        expectedDiscount += 1000; // 크리스마스 당일은 특별 할인 적용 o
+        int expectedDiscount = 0;
+        expectedDiscount += 1000 + 100 * (dayOfMonth - 1); // 크리스마스 디데이 할인(2023.12.1 ~ 25) 적용 o
+        expectedDiscount += 2023 * order.getTotalQuantityByCategory(Category.DESSERT); // 주중 할인(일~목, 디저트) o
+        expectedDiscount += 2023 * order.getTotalQuantityByCategory(Category.MAIN); // 주말 할인(금~토, 메인) x
+        expectedDiscount += 1000; // 특별 할인(일, 크리스마스) 적용 o
         // when
-        int actualDiscount = DiscountPolicy.CHRISTMAS_D_DAY_DISCOUNT.calculateDiscount(order, calendar);
-        actualDiscount += DiscountPolicy.WEEKDAY_DISCOUNT.calculateDiscount(order, calendar);
-        actualDiscount += DiscountPolicy.WEEKEND_DISCOUNT.calculateDiscount(order, calendar);
-        actualDiscount += DiscountPolicy.SPECIAL_DISCOUNT.calculateDiscount(order, calendar);
+        int actualDiscount = 0;
+        for(DiscountPolicy discountPolicy : DiscountPolicy.values()) {
+            actualDiscount += discountPolicy.calculateDiscount(order, calendar);
+        }
         // then
         assertThat(actualDiscount).isEqualTo(expectedDiscount);
     }
