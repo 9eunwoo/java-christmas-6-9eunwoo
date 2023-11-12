409a846c306d169448bda8fe27c139d564f0a5ac
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-11-12 23:08:37 +0900

    refactor(view): 주문 및 이벤트 혜택 미리보기 출력 기능 정리

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,23 +2,20 @@ package christmas.view;
 
 import christmas.domain.GiftItem;
 import christmas.dto.EventDetailsDTO;
+import java.util.Calendar;
 import java.util.Locale;
 
 public class OutputView {
-    public void printEventDetails(EventDetailsDTO eventDetailsDTO, int date) {
-        System.out.printf("12월 %,d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!%n", date);
+    public void printEventDetails(EventDetailsDTO eventDetailsDTO, Calendar calendar) {
+        int month = calendar.get(Calendar.MONTH) + 1; // Calendar.MONTH starts from 0
+        int date = calendar.get(Calendar.DATE);
+        System.out.printf("%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!%n%n", month, date);
         printOrderItems(eventDetailsDTO);
-        System.out.println();
         printTotalPrice(eventDetailsDTO);
-        System.out.println();
         printGiftItem(eventDetailsDTO);
-        System.out.println();
         printBenefits(eventDetailsDTO);
-        System.out.println();
         printTotalBenefit(eventDetailsDTO);
-        System.out.println();
         printNetPrice(eventDetailsDTO);
-        System.out.println();
         printEventBadge(eventDetailsDTO);
     }
 
@@ -26,21 +23,22 @@ public class OutputView {
         System.out.printf("<주문 메뉴>%n");
         eventDetailsDTO.getOrderItems().forEach(
                 (key, value) -> System.out.printf(Locale.KOREA, "%s %,d개%n", key.getName(), value));
+        System.out.println();
     }
 
     private void printTotalPrice(EventDetailsDTO eventDetailsDTO) {
         System.out.printf("<할인 전 총주문 금액>%n");
-        System.out.printf(Locale.KOREA, "%,d원%n", eventDetailsDTO.getTotalPrice());
+        System.out.printf(Locale.KOREA, "%,d원%n%n", eventDetailsDTO.getTotalPrice());
     }
 
     private void printGiftItem(EventDetailsDTO eventDetailsDTO) {
         System.out.printf("<증정 메뉴>%n");
         GiftItem giftItem = eventDetailsDTO.getGiftItem();
         if (giftItem == GiftItem.NONE) {
-            System.out.printf("없음%n");
+            System.out.printf("없음%n%n");
             return;
         }
-        System.out.printf(Locale.KOREA, "%s %,d개%n", giftItem.getItem().getName(), giftItem.getQuantity());
+        System.out.printf(Locale.KOREA, "%s %,d개%n%n", giftItem.getItem().getName(), giftItem.getQuantity());
     }
 
     private void printBenefits(EventDetailsDTO eventDetailsDTO) {
@@ -59,6 +57,7 @@ public class OutputView {
             System.out.printf(Locale.KOREA, "특별 할인: -%,d원%n", eventDetailsDTO.getSpecialDiscount());
         }
         printGiftItemPrice(eventDetailsDTO);
+        System.out.println();
     }
 
     private void printGiftItemPrice(EventDetailsDTO eventDetailsDTO) {
@@ -71,12 +70,12 @@ public class OutputView {
 
     private void printTotalBenefit(EventDetailsDTO eventDetailsDTO) {
         System.out.printf("<총혜택 금액>%n");
-        System.out.printf(Locale.KOREA, "-%,d원%n", eventDetailsDTO.getTotalBenefit());
+        System.out.printf(Locale.KOREA, "-%,d원%n%n", eventDetailsDTO.getTotalBenefit());
     }
 
     private void printNetPrice(EventDetailsDTO eventDetailsDTO) {
         System.out.printf("<할인 후 예상 결제 금액>%n");
-        System.out.printf(Locale.KOREA, "%,d원%n",
+        System.out.printf(Locale.KOREA, "%,d원%n%n",
                 eventDetailsDTO.getTotalPrice() - eventDetailsDTO.getTotalBenefit());
     }
 
