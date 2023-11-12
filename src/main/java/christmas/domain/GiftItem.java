f3a8470c1a547e45e7fbd82428827aa16474722a
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-11-12 13:23:30 +0900

    feat(domain): 증정 메뉴 수량 추가

--- src/main/java/christmas/domain/GiftItem.java
+++ src/main/java/christmas/domain/GiftItem.java
@@ -1,19 +1,25 @@
 package christmas.domain;
 
 public enum GiftItem {
-    NONE(null, 0),
-    CHAMPAGNE(MenuItem.CHAMPAGNE, 120_000);
+    NONE(null, 0, 0),
+    CHAMPAGNE(MenuItem.CHAMPAGNE, 1, 120_000);
 
     private final MenuItem item;
+    private final int quantity;
     private final int threshold;
 
-    GiftItem(MenuItem item, int threshold) {
+    GiftItem(MenuItem item, int quantity, int threshold) {
         this.item = item;
+        this.quantity = quantity;
         this.threshold = threshold;
     }
 
     public MenuItem getItem() {
         return item;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 
     public static GiftItem fromTotalPrice(int totalPrice) {
