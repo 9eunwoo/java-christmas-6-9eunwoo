032669070e284a73ca114fe69e1757c9b6cbf2eb
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-11-12 23:30:39 +0900

    feat(controller): 주문 및 이벤트 서비스 진행 기능 구현

---
+++ src/main/java/christmas/controller/EventController.java
@@ -0,0 +1,43 @@
+package christmas.controller;
+
+import christmas.domain.Order;
+import christmas.dto.EventDetailsDTO;
+import christmas.service.EventService;
+import christmas.service.OrderService;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+import java.util.Calendar;
+
+public class EventController {
+    private final int YEAR = 2023;
+    private final int MONTH = 12;
+    private final EventService eventService;
+    private final OrderService orderService;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public EventController() {
+        this.eventService = new EventService();
+        this.orderService = new OrderService();
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void start() {
+        outputView.printWelcomeMessage();
+        int date = inputView.readDate();
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(YEAR, MONTH, date);
+        while (true) {
+            try {
+                String orderInput = inputView.readOrder();
+                Order order = orderService.takeOrder(orderInput);
+                EventDetailsDTO eventDetailsDTO = eventService.getEventDetails(order, calendar);
+                outputView.printEventDetails(eventDetailsDTO, calendar);
+                return;
+            } catch (IllegalArgumentException | IllegalStateException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+    }
+}
