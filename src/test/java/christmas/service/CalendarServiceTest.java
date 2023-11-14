c9cccda00b27fdd76275e185d4ba4af02b197eb9
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-11-14 17:53:27 +0900

    test(service): 유효하지 않은 날짜를 입력할 경우 예외 처리 테스트

---
+++ src/test/java/christmas/service/CalendarServiceTest.java
@@ -0,0 +1,22 @@
+package christmas.service;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalendarServiceTest {
+    @DisplayName("유효하지 않은 날짜를 입력할 경우, 예외를 발생시키는지 테스트")
+    @Test
+    void getValidCalendar_ShouldThrowException_WhenInvalidDate() {
+        // given
+        int year = 2023;
+        int month = 12;
+        int date = 32;
+        CalendarService calendarService = new CalendarService();
+
+        // when & then
+        assertThatThrownBy(() -> calendarService.getValidCalendar(year, month, date))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
