9fcd3a1a0604a4403f559c9c9b2775a01ef59fa1
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-11-14 15:14:08 +0900

    style(view): 가독성을 위한 메소드 순서 변경

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -18,24 +18,9 @@ public class InputView {
                 validateNonZeroLeadingUpToTwoDigits(userInput);
                 int date = Integer.parseInt(userInput);
                 return getValidCalendar(date);
-            } catch (IllegalArgumentException dateException) {
+            } catch (IllegalArgumentException dateInputException) {
                 System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-                System.out.println(dateException.getMessage());
-            }
-        }
-    }
-
-    public String readOrderInput() {
-        while (true) {
-            try {
-                System.out.println(
-                        "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-                String userInput = Console.readLine();
-                validateOrderInput(userInput);
-                return userInput;
-            } catch (IllegalArgumentException orderInputException) {
-                System.out.println("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-                System.out.println(orderInputException.getMessage());
+                System.out.println(dateInputException.getMessage());
             }
         }
     }
@@ -54,6 +39,21 @@ public class InputView {
             throw new IllegalArgumentException("[ERROR] 날짜는 1일부터 31일 사이로 입력해 주세요.");
         }
         return calendar;
+    }
+
+    public String readOrderInput() {
+        while (true) {
+            try {
+                System.out.println(
+                        "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+                String userInput = Console.readLine();
+                validateOrderInput(userInput);
+                return userInput;
+            } catch (IllegalArgumentException orderInputException) {
+                System.out.println("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+                System.out.println(orderInputException.getMessage());
+            }
+        }
     }
 
     private void validateOrderInput(String userInput) {
