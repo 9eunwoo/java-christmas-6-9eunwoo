6ae0195c8c3a880b32ae238f2659258778ddeb68
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-11-15 18:19:53 +0900

    test(domain): 총 구매 금액에 따른 할인 적용 여부 테스트

--- src/test/java/christmas/domain/DiscountPolicyTest.java
+++ src/test/java/christmas/domain/DiscountPolicyTest.java
@@ -15,17 +15,63 @@ public class DiscountPolicyTest {
     void givenDiscountPolicy_whenCalculateDiscount_thenCorrectDiscountCalculated() {
         // given
         Calendar calendar = Calendar.getInstance();
-        int dayOfMonth = 25;
-        calendar.set(2023, Calendar.DECEMBER, dayOfMonth); // 2023.12.25 (월) 크리스마스
+        int date = 25;
+        calendar.set(2023, Calendar.DECEMBER, date); // 2023.12.25 (월) 크리스마스
         MenuItem item = MenuItem.CHOCO_CAKE; // 초코케이크는 디저트
         int quantity = 2;
         Map<MenuItem, Integer> orderItems = new EnumMap<>(MenuItem.class);
         orderItems.put(item, quantity);
         Order order = Order.create(orderItems);
         int expectedDiscount = 0;
-        expectedDiscount += 1000 + 100 * (dayOfMonth - 1); // 크리스마스 디데이 할인(2023.12.1 ~ 25) 적용 o
-        expectedDiscount += 2023 * order.calculateTotalQuantityByCategory(Category.DESSERT); // 주중 할인(일~목, 디저트) o
-        expectedDiscount += 2023 * order.calculateTotalQuantityByCategory(Category.MAIN); // 주말 할인(금~토, 메인) x
+        expectedDiscount += 1000 + 100 * (date - 1); // 크리스마스 디데이 할인(2023.12.1 ~ 25) 적용 o
+        expectedDiscount += 2023 * 2; // 주중 할인(일~목, 디저트) o
+        expectedDiscount += 1000; // 특별 할인(일, 크리스마스) 적용 o
+        // when
+        int actualDiscount = 0;
+        for (DiscountPolicy discountPolicy : DiscountPolicy.values()) {
+            actualDiscount += discountPolicy.calculateDiscount(calendar, order);
+        }
+        // then
+        assertThat(actualDiscount).isEqualTo(expectedDiscount);
+    }
+
+    @DisplayName("총 구매 금액이 10,000원 미만일 때 할인이 적용되지 않는지 확인한다.")
+    @Test
+    void givenTotalPriceLessThanDiscountThreshold_whenCalculateDiscount_thenNoDiscount() {
+        // given
+        Calendar calendar = Calendar.getInstance();
+        int date = 25;
+        calendar.set(2023, Calendar.DECEMBER, date); // 2023.12.25 (월) 크리스마스
+        MenuItem item = MenuItem.CAESAR_SALAD; // 시저샐러드 가격은 8,000원
+        int quantity = 1;
+        Map<MenuItem, Integer> orderItems = new EnumMap<>(MenuItem.class);
+        orderItems.put(item, quantity);
+        Order order = Order.create(orderItems);
+        int expectedDiscount = 0;
+        // when
+        int actualDiscount = 0;
+        for (DiscountPolicy discountPolicy : DiscountPolicy.values()) {
+            actualDiscount += discountPolicy.calculateDiscount(calendar, order);
+        }
+        // then
+        assertThat(actualDiscount).isEqualTo(expectedDiscount);
+    }
+
+    @DisplayName("총 구매 금액이 10,000원 이상일 때 할인이 적용되는지 확인한다.")
+    @Test
+    void givenTotalPriceMoreThanDiscountThreshold_whenCalculateDiscount_thenDiscount() {
+        // given
+        Calendar calendar = Calendar.getInstance();
+        int date = 25;
+        calendar.set(2023, Calendar.DECEMBER, date); // 2023.12.25 (월) 크리스마스
+        MenuItem item = MenuItem.ICE_CREAM; // 아이스크림는 디저트이고 가격은 5,000원
+        int quantity = 2;
+        Map<MenuItem, Integer> orderItems = new EnumMap<>(MenuItem.class);
+        orderItems.put(item, quantity);
+        Order order = Order.create(orderItems);
+        int expectedDiscount = 0;
+        expectedDiscount += 1000 + 100 * (date - 1); // 크리스마스 디데이 할인(2023.12.1 ~ 25) 적용 o
+        expectedDiscount += 2023 * 2; // 주중 할인(일~목, 디저트) o
         expectedDiscount += 1000; // 특별 할인(일, 크리스마스) 적용 o
         // when
         int actualDiscount = 0;
